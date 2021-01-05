49f319f4934143b154caaa665152b685302a17d7
rlc900 <rianna.cleary@gmail.com>
2021-01-05 11:50:06 -0500

    create menu data and rendered header

---
+++ src/data/MenuData.js
@@ -0,0 +1,20 @@
+export const menuData = [
+    {
+    
+        title: "About", 
+        icon: "/images/icons/courses.svg", 
+        link: "/courses"
+    }, 
+    {
+        title: "Projects", 
+        icon: "/images/icons/tutorials.svg", 
+        link: "/tutorials"
+    
+    },
+    {
+        title: "Contact Info", 
+        icon: "/images/icons/pricing.svg", 
+        link: "/pricing"
+    }
+    
+    ];
