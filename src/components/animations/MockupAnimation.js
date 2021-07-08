fd4196b98736b05f4d73119c8a25c3fa38a0eaa3
rlc900 <rianna.cleary@gmail.com>
2021-07-08 10:05:08 -0400

    added medium image

--- src/components/animations/MockupAnimation.js
+++ src/components/animations/MockupAnimation.js
@@ -87,24 +87,29 @@ const Wrapper = styled.div`
 
     .mockup1 {
         position: absolute;
-        width: 183px;
-        height: 120px;
+        /* width: 183px;
+        height: 120px; */
+        width: 150px;
+        height: 150px;
         right: 617px;
         top: 0px;
 
-        background: url('/images/animations/mockup1.svg'), radial-gradient(
+        background: url('/images/animations/medium2.png'), radial-gradient(
           218.51% 281.09% at 100% 100%,
           rgba(253, 63, 51, 0.6) 0%,
           rgba(76, 0, 200, 0.6) 45.83%,
           rgba(76, 0, 200, 0.6) 100%
         );
+        background-size: 160px;
+        background-position: center;
         border: 0.273134px solid rgba(255, 255, 255, 0.3);
         box-sizing: border-box;
         box-shadow: 0px 16.3881px 32.7761px rgba(99, 30, 187, 0.5);
         backdrop-filter: blur(21.8507px);
         /* Note: backdrop-filter has minimal browser support */
 
-        border-radius: 16.3881px;
+        /* border-radius: 16.3881px; */
+        border-radius: 50%;
 
     }
 
