a0c3e64bf13917420c204d46a4536ccad7fbe7f5
Rianna <rianna.cleary@gmail.com>
2023-01-08 21:36:11 -0500

    light & dark mode persists on refresh

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -16,13 +16,21 @@ function HeroSection() {
   const isDarkTheme = theme === 'dark';
 
   const toggleTheme = () => {
-    setTheme(isDarkTheme ? 'light' : 'dark');
-    console.log('hi')
+    const persistingTheme = isDarkTheme ? 'light' : 'dark';
+    setTheme(persistingTheme);
+    localStorage.setItem('theme', persistingTheme);
   };
 
-// useEffect(() => {
-//   document.body.className = theme;
-// }, [theme]);
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    const prefersDark = window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (savedTheme && ['dark', 'light'].includes(savedTheme)) {
+      setTheme(savedTheme);
+    } else if (prefersDark) {
+      setTheme('dark')
+    }
+  }, []);
 
     return (
       <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
